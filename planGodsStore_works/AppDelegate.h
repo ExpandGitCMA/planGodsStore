1a781dc58b99afd7cac43a1b081f236ceefd6081
景阳 <2966497308@qq.com>
2017-06-06 18:04:50 +0800

    Initial Commit

---
+++ planGodsStore_works/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  planGodsStore_works
+//
+//  Created by ZeroSmile on 2017/6/6.
+//  Copyright © 2017年 陈美安. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
